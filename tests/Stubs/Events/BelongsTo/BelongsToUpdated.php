9840a63e85f46eddfc79b5707d3372946350feea
Viacheslav Ostrovskiy <chelout@gmail.com>
2018-09-17 10:09:14 +0300

    Added HasBelongsToEventsTest for $dispatchesEvents

---
+++ tests/Stubs/Events/BelongsTo/BelongsToUpdated.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo;
+
+class BelongsToUpdated
+{
+    public $user;
+    public $relation;
+    public $profile;
+
+    public function __construct($profile, $params)
+    {
+        $this->profile = $profile;
+        $this->relation = $params[0];
+        $this->user = $params[1];
+    }
+}
--- tests/Stubs/Profile.php
+++ tests/Stubs/Profile.php
@@ -3,14 +3,31 @@
 namespace Chelout\RelationshipEvents\Tests\Stubs;
 
 use Chelout\RelationshipEvents\Concerns\HasBelongsToEvents;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo\BelongsToAssociated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo\BelongsToAssociating;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo\BelongsToDissociated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo\BelongsToDissociating;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo\BelongsToUpdated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsTo\BelongsToUpdating;
+use Chelout\RelationshipEvents\Traits\HasDispatchableEvents;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 
 class Profile extends Model
 {
-    use HasBelongsToEvents;
+    use HasDispatchableEvents,
+        HasBelongsToEvents;
 
     protected $guarded = [];
+
+    protected $dispatchesEvents = [
+        'belongsToAssociating' => BelongsToAssociating::class,
+        'belongsToAssociated' => BelongsToAssociated::class,
+        'belongsToDissociating' => BelongsToDissociating::class,
+        'belongsToDissociated' => BelongsToDissociated::class,
+        'belongsToUpdating' => BelongsToUpdating::class,
+        'belongsToUpdated' => BelongsToUpdated::class,
+    ];
 
     public static function setupTable()
     {
