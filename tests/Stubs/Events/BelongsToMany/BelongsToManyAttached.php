f7af9b3cf87bca0f40b99c87a4af6d16b86eabbd
Viacheslav Ostrovskiy <chelout@gmail.com>
2018-09-18 10:23:40 +0300

    Added HasBelongsToManyEventsTest for $dispatchesEvents

---
+++ tests/Stubs/Events/BelongsToMany/BelongsToManyAttached.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany;
+
+class BelongsToManyAttached
+{
+    public $user;
+    public $relation;
+    public $roleId;
+    public $attributes;
+
+    public function __construct($user, $params)
+    {
+        $this->user = $user;
+        $this->relation = $params[0];
+        $this->roleId = $params[1][0];
+        $this->attributes = $params[2];
+    }
+}
--- tests/Stubs/User.php
+++ tests/Stubs/User.php
@@ -6,6 +6,16 @@ use Chelout\RelationshipEvents\Concerns\HasBelongsToManyEvents;
 use Chelout\RelationshipEvents\Concerns\HasManyEvents;
 use Chelout\RelationshipEvents\Concerns\HasMorphOneEvents;
 use Chelout\RelationshipEvents\Concerns\HasOneEvents;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyAttached;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyAttaching;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyDetached;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyDetaching;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManySynced;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManySyncing;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyToggled;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyToggling;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyUpdatedExistingPivot;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\BelongsToMany\BelongsToManyUpdatingExistingPivot;
 use Chelout\RelationshipEvents\Tests\Stubs\Events\HasMany\HasManyCreated;
 use Chelout\RelationshipEvents\Tests\Stubs\Events\HasMany\HasManyCreating;
 use Chelout\RelationshipEvents\Tests\Stubs\Events\HasMany\HasManySaved;
@@ -45,6 +55,17 @@ class User extends Model
         'hasManySaved' => HasManySaved::class,
         'hasManyUpdating' => HasManyUpdating::class,
         'hasManyUpdated' => HasManyUpdated::class,
+        // BelongsToMany
+        'belongsToManyAttaching' => BelongsToManyAttaching::class,
+        'belongsToManyAttached' => BelongsToManyAttached::class,
+        'belongsToManyDetaching' => BelongsToManyDetaching::class,
+        'belongsToManyDetached' => BelongsToManyDetached::class,
+        'belongsToManySyncing' => BelongsToManySyncing::class,
+        'belongsToManySynced' => BelongsToManySynced::class,
+        'belongsToManyToggling' => BelongsToManyToggling::class,
+        'belongsToManyToggled' => BelongsToManyToggled::class,
+        'belongsToManyUpdatingExistingPivot' => BelongsToManyUpdatingExistingPivot::class,
+        'belongsToManyUpdatedExistingPivot' => BelongsToManyUpdatedExistingPivot::class,
     ];
 
     public static function setupTable()
