6715d9380fb8bfacbb4ce417773af87f3b2c7eef
Viacheslav Ostrovskiy <chelout@gmail.com>
2018-09-21 08:51:22 +0300

    Added HasMorphManyEventsTest for $dispatchesEvents

---
+++ tests/Feature/DispatchesEvents/HasMorphManyEventsViaDispatchesEventsTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Chelout\RelationshipEvents\Tests\Feature\DispatchesEvents;
+
+use Chelout\RelationshipEvents\Tests\Stubs\Comment;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyCreated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyCreating;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManySaved;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManySaving;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyUpdated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyUpdating;
+use Chelout\RelationshipEvents\Tests\Stubs\Post;
+use Chelout\RelationshipEvents\Tests\TestCase;
+use Illuminate\Support\Facades\Event;
+
+class HasMorphManyEventsViaDispatchesEventsTest extends TestCase
+{
+    public function setup()
+    {
+        parent::setup();
+
+        Post::setupTable();
+        Comment::setupTable();
+    }
+
+    /** @test */
+    public function it_fires_morphManyCreating_and_morphManyCreated_when_belonged_model_with_morph_many_created()
+    {
+        Event::fake();
+
+        $post = Post::create(['user_id' => 1]);
+        $comment = $post->comments()->create([]);
+
+        Event::assertDispatched(
+            MorphManyCreating::class,
+            function ($event) use ($post, $comment) {
+                return $event->post->is($post) && $event->comment->is($comment);
+            }
+        );
+        Event::assertDispatched(
+            MorphManyCreated::class,
+            function ($event) use ($post, $comment) {
+                return $event->post->is($post) && $event->comment->is($comment);
+            }
+        );
+    }
+
+    /** @test */
+    public function it_fires_morphManySaving_and_morphManySaved_when_belonged_model_with_morph_many_saved()
+    {
+        Event::fake();
+
+        $post = Post::create(['user_id' => 1]);
+        $comment = $post->comments()->save(new Comment);
+
+        Event::assertDispatched(
+            MorphManySaving::class,
+            function ($event) use ($post, $comment) {
+                return $event->post->is($post) && $event->comment->is($comment);
+            }
+        );
+        Event::assertDispatched(
+            MorphManySaved::class,
+            function ($event) use ($post, $comment) {
+                return $event->post->is($post) && $event->comment->is($comment);
+            }
+        );
+    }
+
+    /** @test */
+    public function it_fires_morphManyUpdating_and_morphManyUpdated_when_belonged_model_with_morph_many_updated()
+    {
+        Event::fake();
+
+        $post = Post::create(['user_id' => 1]);
+        $comment = $post->comments()->save(new Comment);
+        $post->comments()->update([]);
+
+        Event::assertDispatched(
+            MorphManyUpdating::class,
+            function ($event) use ($post, $comment) {
+                return $event->post->is($post);
+            }
+        );
+        Event::assertDispatched(
+            MorphManyUpdated::class,
+            function ($event) use ($post, $comment) {
+                return $event->post->is($post) && $event->comment[0]->is($comment);
+            }
+        );
+    }
+}
--- tests/Stubs/Post.php
+++ tests/Stubs/Post.php
@@ -4,15 +4,33 @@ namespace Chelout\RelationshipEvents\Tests\Stubs;
 
 use Chelout\RelationshipEvents\Concerns\HasMorphManyEvents;
 use Chelout\RelationshipEvents\Concerns\HasMorphToManyEvents;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyCreated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyCreating;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManySaved;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManySaving;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyUpdated;
+use Chelout\RelationshipEvents\Tests\Stubs\Events\MorphMany\MorphManyUpdating;
+use Chelout\RelationshipEvents\Traits\HasDispatchableEvents;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 
 class Post extends Model
 {
-    use HasMorphManyEvents,
+    use HasDispatchableEvents,
+        HasMorphManyEvents,
         HasMorphToManyEvents;
 
     protected $guarded = [];
+
+    protected $dispatchesEvents = [
+        // MorphMany
+        'morphManyCreating' => MorphManyCreating::class,
+        'morphManyCreated' => MorphManyCreated::class,
+        'morphManySaving' => MorphManySaving::class,
+        'morphManySaved' => MorphManySaved::class,
+        'morphManyUpdating' => MorphManyUpdating::class,
+        'morphManyUpdated' => MorphManyUpdated::class,
+    ];
 
     public static function setupTable()
     {
